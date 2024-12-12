c09fe5a1cb85b65bb483802e83a7439d95182c7f
Andrew Todd <andrew.s.todd@wsu.edu>
2024-12-11 19:24:15 -0800

    finished 3a

---
+++ 3a/main.py
@@ -0,0 +1,16 @@
+from functools import reduce
+import re
+
+def main():
+    with open("input.txt", "r") as file:
+        input = file.read()
+        filtered = re.findall(r"\bmul\b\([\d)]+[,][\d]+\)", input)
+
+        numbers = [re.findall(r"(\d+)", s) for s in filtered]
+        
+        total = reduce(lambda i, j: i + j, list(map(lambda a: reduce(lambda x, y: int(x) * int(y), a), numbers)))
+
+        print(f"Total: ", total)
+
+if __name__ == "__main__":
+    main()
